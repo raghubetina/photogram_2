75fa0f0fadf002734aa9920f71617ed7a596803d
Raghu Betina <raghu@firstdraft.com>
2020-10-08 17:02:51 +0000

    Add doorkeeper before action hook

--- app/controllers/api/v1/graphiti_controller.rb
+++ app/controllers/api/v1/graphiti_controller.rb
@@ -1,5 +1,13 @@
 class Api::V1::GraphitiController < ApplicationController
+  skip_before_action :authenticate_user!
+  before_action -> { doorkeeper_authorize! :public }
+
 
     include Graphiti::Rails::Responders
+
+  def current_resource_owner
+    doorkeeper_token.resource_owner
+  end
+
 
 end
