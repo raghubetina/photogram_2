9f6cd6cf1990bcce7202899ebadbf5210773121d
Raghu Betina <raghu@firstdraft.com>
2020-10-08 17:01:20 +0000

    Direct association between photos and votes

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,6 +1,10 @@
 class Photo < ApplicationRecord
   # Direct associations
 
+  has_many   :likes,
+             :class_name => "Vote",
+             :dependent => :destroy
+
   belongs_to :owner,
              :class_name => "User",
              :counter_cache => true
